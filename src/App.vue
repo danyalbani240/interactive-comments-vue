ba4c0496dc6d15cf03ad0f8d9edd3d817726d6b0
Danyal <mr.d.bani.d@gmail.com>
2022-07-12 19:29:04 +0330

    add comment box added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,44 @@
 <template>
-	<div></div>
+	<div class="mx-auto flex flex-col justify-center items-center">
+		<Comment
+			v-for="comment in comments"
+			:commentData="comment"
+			:key="comment.id"
+		/>
+	</div>
+	<div
+		class="bg-white py-4 mx-auto rounded flex flex-col w-11/12 mt-5 max-w-3xl add-comment-container"
+	>
+		<textarea
+			placeholder="Add a Comment"
+			name="Add a Comment"
+			class="rounded border-2 add-comment-input w-11/12 mx-auto border-r-gray-400 resize-none outline-none px-2 focus:border-gray-600 py-2 user-new-comment-input"
+			id=""
+			cols="30"
+			rows="5"
+		></textarea>
+		<div
+			class="flex items-center mt-6 justify-between px-2 w-11/12 mx-auto"
+		>
+			<img
+				class="w-9"
+				src="https://i.ibb.co/jWJfdwM/image-juliusomo.png"
+				alt="account owner image"
+			/>
+			<button
+				data-test="send-comment"
+				class="text-white add-new-comment bg-purple-700 rounded px-4 py-2 text-sm h-10 font-bold"
+			>
+				Send
+			</button>
+		</div>
+	</div>
 </template>
 
 <script>
+import Comment from "./components/Comment.vue";
 export default {
+	components: { Comment },
 	data() {
 		return {
 			comments: [],
@@ -21,12 +56,12 @@ export default {
 };
 </script>
 <style>
-#app {
+/* #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
-}
+} */
 </style>
