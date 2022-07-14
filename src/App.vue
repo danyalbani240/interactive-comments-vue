c111887ef49231d55b6f83773ab306b5d976858c
Danyal <mr.d.bani.d@gmail.com>
2022-07-14 20:05:17 +0330

    adding new Commen

--- src/App.vue
+++ src/App.vue
@@ -8,39 +8,14 @@
 			@delete-reply="deleteReply"
 		/>
 	</div>
-	<div
-		class="bg-white py-4 mx-auto rounded flex flex-col w-11/12 mt-5 max-w-3xl add-comment-container"
-	>
-		<textarea
-			placeholder="Add a Comment"
-			name="Add a Comment"
-			class="rounded border-2 add-comment-input w-11/12 mx-auto border-r-gray-400 resize-none outline-none px-2 focus:border-gray-600 py-2 user-new-comment-input"
-			id=""
-			cols="30"
-			rows="5"
-		></textarea>
-		<div
-			class="flex items-center mt-6 justify-between px-2 w-11/12 mx-auto"
-		>
-			<img
-				class="w-9"
-				src="https://i.ibb.co/jWJfdwM/image-juliusomo.png"
-				alt="account owner image"
-			/>
-			<button
-				data-test="send-comment"
-				class="text-white add-new-comment bg-purple-700 rounded px-4 py-2 text-sm h-10 font-bold"
-			>
-				Send
-			</button>
-		</div>
-	</div>
+	<AddNewCommentForm @createReply="createReply" />
 </template>
 
 <script>
 import Comment from "./components/Comment.vue";
+import AddNewCommentForm from "./components/AddNewCommentForm.vue";
 export default {
-	components: { Comment },
+	components: { Comment, AddNewCommentForm },
 	data() {
 		return {
 			comments: [],
@@ -123,6 +98,36 @@ export default {
 						replyIndex,
 						1
 					);
+				})
+				.catch((e) => console.log(e));
+		},
+		createReply(data) {
+			fetch(
+				"https://interactive-comments-70a95-default-rtdb.firebaseio.com/comments.json",
+				{
+					method: "POST",
+					body: JSON.stringify(data),
+					headers: {
+						"Content-type": "application/json; charset=UTF-8",
+					},
+				}
+			)
+				.then((res) => res.json())
+				.then(({ name }) => {
+					this.comments.push({ ...data, id: name });
+					fetch(
+						"https://interactive-comments-70a95-default-rtdb.firebaseio.com/comments/" +
+							name +
+							".json",
+						{
+							method: "PATCH",
+							body: JSON.stringify({ id: name }),
+							headers: {
+								"Content-type":
+									"application/json; charset=UTF-8",
+							},
+						}
+					).catch((e) => console.log(e));
 				})
 				.catch((e) => console.log(e));
 		},
---
+++ src/components/AddNewCommentForm.vue
@@ -0,0 +1,61 @@
+<template>
+	<form
+		@submit.prevent="handleSubmit"
+		class="bg-white py-4 mx-auto rounded flex flex-col w-11/12 mt-5 max-w-3xl add-comment-container"
+	>
+		<textarea
+			placeholder="Add a Comment"
+			name="Add a Comment"
+			class="rounded border-2 add-comment-input w-11/12 mx-auto border-r-gray-400 resize-none outline-none px-2 focus:border-gray-600 py-2 user-new-comment-input"
+			id=""
+			cols="30"
+			rows="5"
+			v-model="content"
+		></textarea>
+		<div
+			class="flex items-center mt-6 justify-between px-2 w-11/12 mx-auto"
+		>
+			<img
+				class="w-9"
+				src="https://i.ibb.co/jWJfdwM/image-juliusomo.png"
+				alt="account owner image"
+			/>
+			<button
+				data-test="send-comment"
+				class="text-white add-new-comment bg-purple-700 rounded px-4 py-2 text-sm h-10 font-bold"
+			>
+				Send
+			</button>
+		</div>
+	</form>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			content: "",
+		};
+	},
+	methods: {
+		handleSubmit() {
+			const data = {
+				content: this.content,
+				score: 0,
+				replies: [],
+				user: {
+					username: "juliusomo",
+					image: {
+						png: "https://i.ibb.co/jWJfdwM/image-juliusomo.png",
+						webp: "https://i.ibb.co/cDyZ7yQ/image-juliusomo.webp",
+					},
+				},
+				createdAt: "1 day ago",
+			};
+			this.$emit("createReply", data);
+		},
+	},
+};
+</script>
+
+<style></style>
