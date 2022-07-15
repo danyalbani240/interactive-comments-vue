140f2c066eb3b2727f4c69667ea9b2a3ce80f4ed
Danyal <mr.d.bani.d@gmail.com>
2022-07-15 15:40:50 +0330

    edit button hitted?if there is a change emit submitModal Event with new Data

--- src/components/EditModal.vue
+++ src/components/EditModal.vue
@@ -6,19 +6,22 @@
 	>
 		<form
 			class="fixed z-10 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-72 md:w-6/12 h-44 bg-white rounded p-4 edit-reply-box"
-			@submit.prevent="this.$emit('handleEdit')"
+			@submit.prevent=""
 		>
 			<textarea
 				class="w-full h-20 border-gray-200 focus:border-gray-400 outline-none rounded px-4 py-2 border-2 resize-none my-3"
 				placeholder="Reply Text"
+				v-model="newContent"
 			></textarea>
 			<div class="flex justify-between">
 				<button
 					class="text-white bg-gray-600 cancel p-2 mt-2 cursor-pointer rounded"
+					@click="this.$emit('cancel')"
 				>
 					CANCEL</button
 				><button
 					class="text-white bg-purple-600 p-2 mt-2 send-edit cursor-pointer rounded"
+					@click="handleEdit"
 				>
 					Edit
 				</button>
@@ -30,9 +33,24 @@
 <script>
 export default {
 	props: {
-		name: {
+		baseValue: {
 			type: String,
 			required: true,
+		},
+	},
+	data() {
+		return {
+			newContent: this.baseValue,
+		};
+	},
+	methods: {
+		handleEdit() {
+			// check if there is any change
+			if (this.baseValue === this.newContent) {
+				return;
+			} else {
+				this.$emit("submitModal", this.newContent);
+			}
 		},
 	},
 };
