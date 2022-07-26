d554b3bd995a7de56606b91756fb4a0f7c70faa0
Danyal <mr.d.bani.d@gmail.com>
2022-07-27 00:03:20 +0330

    adding loader and vue-lottie to loading animation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-	<div class="mx-auto flex flex-col justify-center items-center">
+	<div v-if="commentsStore.comments.length === 0">
+		<Vue3Lottie :animationData="loader" :height="200" :width="200" />
+	</div>
+
+	<div v-else class="mx-auto flex flex-col justify-center items-center">
 		<Comment
 			v-for="comment in commentsStore.comments"
 			:commentData="comment"
@@ -12,14 +16,23 @@
 
 <script>
 import Comment from "./components/Comment.vue";
+import { Vue3Lottie } from "vue3-lottie";
+import "vue3-lottie/dist/style.css";
+import loader from "./assets/11473-loading-spinner.json";
 import AddNewCommentForm from "./components/AddNewCommentForm.vue";
 import { mapStores } from "pinia";
 import { useCommentsStore } from "./stores/comments";
 export default {
-	components: { Comment, AddNewCommentForm },
-
+	components: { Comment, AddNewCommentForm, Vue3Lottie },
+	data() {
+		return {
+			loader,
+		};
+	},
 	mounted() {
-		this.commentsStore.getComments();
+		setTimeout(() => {
+			this.commentsStore.getComments();
+		}, 500);
 	},
 	computed: {
 		...mapStores(useCommentsStore),
